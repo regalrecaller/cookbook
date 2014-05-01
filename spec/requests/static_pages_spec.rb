7ade3391152b8230c6a00438c3235bbaaef08ac6
regalrecaller <regalrecaller@gmail.com>
2014-05-01 11:44:19 -0700

    added guard, edited guardfile

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,17 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-  describe "Home Page" do
+
+	let (:base_title) {"Cookbook Sample App"}
+
+  	describe "Home Page" do
     it "should have the content 'Home'" do
       visit '/static_pages/home'
 	  expect(page).to have_content('Home')
     end
 	it "should have the title 'Home'" do
 		visit '/static_pages/home'
-		expect(page).to have_title("Cookbook Sample App | Home")
+		expect(page).to have_title("#{base_title} | Home")
 	end
   end
   describe "Help Page" do
@@ -20,7 +23,7 @@ describe "Static Pages" do
 	end
 	it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Cookbook Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end	
   end
   describe "About Page" do
@@ -30,7 +33,17 @@ describe "Static Pages" do
 	end
 	it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Cookbook Sample App | About")
+      expect(page).to have_title("#{base_title} | About")
     end
+  end
+  describe "Contact Page" do
+  	it "should have the content 'Contact'" do
+		visit '/static_pages/contact'
+		expect(page).to have_content('Contact')
+	end
+	it "should have the title 'Contact'" do
+   		visit '/static_pages/contact'
+		expect(page).to have_title("#{base_title} | Contact")
+	end	
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
@@ -42,3 +45,12 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 end
+end
+
+Spork.each_run do
+
+end
+
+
+
+
